966fad57e73c5c6b00083e0b35c2dd9c0d6ad9f6
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-03-08 18:49:21 +0100

    invariant mass in zee

--- ATLAS/scripts/invariant_mass_zee.py
+++ ATLAS/scripts/invariant_mass_zee.py
@@ -26,3 +26,32 @@ if ((p_1_ratio < 0.99) | (p_2_ratio > 1.01)).any():
 
 if ((p_1_ratio < 0.99) | (p_2_ratio > 1.01)).any():
     print("WARNING: deviation in p_out calculation")
+
+
+### THETA CMS
+data["theta_1"] = 2 * np.arctan(np.exp(-data.eta_1))
+data["theta_2"] = 2* np.arctan(np.exp(-data.eta_2))
+
+
+### UNIT VECTORS
+data["unitv_x_1"] = np.sin(data.theta_1) * np.cos(data.phi0_1 * np.pi / 180.0)
+data["unitv_y_1"] = np.sin(data.theta_1) * np.sin(data.phi0_1 * np.pi / 180.0)
+data["unitv_z_1"] = np.cos(data.theta_1)
+
+data["unitv_x_2"] = np.sin(data.theta_2) * np.cos(data.phi0_2 * np.pi / 180.0)
+data["unitv_y_2"] = np.sin(data.theta_2) * np.sin(data.phi0_2 * np.pi / 180.0)
+data["unitv_z_2"] = np.cos(data.theta_2)
+
+### COS
+data["cos"] = data.unitv_x_1 * data.unitv_x_2 + \
+              data.unitv_y_1 * data.unitv_y_2 + \
+              data.unitv_z_1 * data.unitv_z_2
+
+
+### INVARIANT MASS
+mass_electron = 0.000511  # GeV
+data["inv_mass"] = np.sqrt(2 * mass_electron**2 + \
+                           2 * (data.E_1 * data.E_2 - \
+                                np.abs(data.p_1 * data.p_2) * data.cos))
+
+data["inv_mass_me0"] = np.sqrt(2 * np.abs(data.p_1 * data.p_2) * (1 - data.cos))
