cb5851fd4b09ff5999656561b1b7fca725fa2f1e
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-03-12 16:08:45 +0100

    Verbesserungen

--- ATLAS/data/root/calibration/ElecCalib.C
+++ ATLAS/data/root/calibration/ElecCalib.C
@@ -1,4 +1,4 @@
-#include "math.h"
+﻿#include "math.h"
 #include "TMath.h"
 
 double ElecCalib(double e_raw, double pt, double eta, double phi,
@@ -6,17 +6,17 @@ double ElecCalib(double e_raw, double pt, double eta, double phi,
 {
     double energy = e_raw;
     
-    // first phi iteration
-    if (phi > 0. && phi < 0.4) energy *= 91.19 / 89.81;
-    if (phi > 4. && phi < 0.8) energy *= 91.19 / 89.98;
-    if (phi > 0.8 && phi < 1.2) energy *= 91.19 / 89.86;
-    if (phi > 1.2 && phi < 1.6) energy *= 91.19 / 90.29;
-    if (phi > 1.6 && phi < 2.0) energy *= 91.19 / 89.76;
-    if (phi > 2.0 && phi < 2.4) energy *= 91.19 / 89.68;
-    if (phi > 2.4 && phi < 2.8) energy *= 91.19 / 90.07;
-    if (phi > 2.8 && phi < 3.2) energy *= 91.19 / 90.15;
+    // Erste Iteration: Azimuthalwinkel
+    if (phi >= 0. && phi < 0.4) energy *= 91.19 / 89.81;
+    if (phi >= 4. && phi < 0.8) energy *= 91.19 / 89.98;
+    if (phi >= 0.8 && phi < 1.2) energy *= 91.19 / 89.86;
+    if (phi >= 1.2 && phi < 1.6) energy *= 91.19 / 90.29;
+    if (phi >= 1.6 && phi < 2.0) energy *= 91.19 / 89.76;
+    if (phi >= 2.0 && phi < 2.4) energy *= 91.19 / 89.68;
+    if (phi >= 2.4 && phi < 2.8) energy *= 91.19 / 90.07;
+    if (phi >= 2.8 && phi < 3.2) energy *= 91.19 / 90.15;
     
-    // first energy and eta iteration
+    // Erste Iteration: Energie und Pseudorapiditaet
     if (energy < 50.0) {
         if (fabs(eta) >= 0.0 && fabs(eta) < 0.5)
             energy *= 91.19 / 88.83;
@@ -48,43 +48,43 @@ double ElecCalib(double e_raw, double pt, double eta, double phi,
             energy *= 91.19 / 78.87;
     }
     
-    // second phi iteration
-    if (phi > 0. && phi < 0.4) energy *= 91.19 / 90.35;
-    if (phi > 4. && phi < 0.8) energy *= 91.19 / 89.71;
-    if (phi > 0.8 && phi < 1.2) energy *= 91.19 / 90.34;
-    if (phi > 1.2 && phi < 1.6) energy *= 91.19 / 90.18;
-    if (phi > 1.6 && phi < 2.0) energy *= 91.19 / 90.30;
-    if (phi > 2.0 && phi < 2.4) energy *= 91.19 / 90.29;
-    if (phi > 2.4 && phi < 2.8) energy *= 91.19 / 90.27;
-    if (phi > 2.8 && phi < 3.2) energy *= 91.19 / 90.35;
+    // Zweite Iteration: Azimuthalwinkel
+    if (phi >= 0. && phi < 0.4) energy *= 91.19 / 90.35;
+    if (phi >= 4. && phi < 0.8) energy *= 91.19 / 89.71;
+    if (phi >= 0.8 && phi < 1.2) energy *= 91.19 / 90.34;
+    if (phi >= 1.2 && phi < 1.6) energy *= 91.19 / 90.18;
+    if (phi >= 1.6 && phi < 2.0) energy *= 91.19 / 90.30;
+    if (phi >= 2.0 && phi < 2.4) energy *= 91.19 / 90.29;
+    if (phi >= 2.4 && phi < 2.8) energy *= 91.19 / 90.27;
+    if (phi >= 2.8 && phi < 3.2) energy *= 91.19 / 90.35;
     
-    // second eta iteration
-    if (fabs(eta) > 0.0 && fabs(eta) < 1.0)
+    // Zweite Iteration: Pseudorapiditaet
+    if (fabs(eta) >= 0.0 && fabs(eta) < 1.0)
         energy *= 91.19 / 92.19;
-    if (fabs(eta) > 1.0 && fabs(eta) < 1.5)
+    if (fabs(eta) >= 1.0 && fabs(eta) < 1.5)
         energy *= 91.19 / 90.54;
-    if (fabs(eta) > 1.5 && fabs(eta) < 2.0)
+    if (fabs(eta) >= 1.5 && fabs(eta) < 2.0)
         energy *= 91.19 / 88.12;
-    if (fabs(eta) > 2.0 && fabs(eta) < 2.5)
+    if (fabs(eta) >= 2.0 && fabs(eta) < 2.5)
         energy *= 91.19 / 86.56;
-  
-    // second energy iteration
-    if (energy > 0.0 && energy < 50.0) energy *= 91.19 / 91.81;
-    if (energy > 50.0 && energy < 100.0) energy *= 91.19 / 91.04;
-    if (energy > 100.0 && energy < 150.0) energy *= 91.19 / 90.26;
-    if (energy > 150.0) energy *= 91.19 / 89.16;
     
-    // third eta iteration
-    if (fabs(eta) > 0.0 && fabs(eta) < 0.75)
+    // Zweite Iteration: Energie
+    if (energy >= 0.0 && energy < 50.0) energy *= 91.19 / 91.81;
+    if (energy >= 50.0 && energy < 100.0) energy *= 91.19 / 91.04;
+    if (energy >= 100.0 && energy < 150.0) energy *= 91.19 / 90.26;
+    if (energy >= 150.0) energy *= 91.19 / 89.16;
+    
+    // Dritte Iteration: Pseudorapiditaet
+    if (fabs(eta) >= 0.0 && fabs(eta) < 0.75)
         energy *= 91.19 / 92.06;
-    if (fabs(eta) > 0.75 && fabs(eta) < 1.5)
+    if (fabs(eta) >= 0.75 && fabs(eta) < 1.5)
         energy *= 91.19 / 91.16;
-    if (fabs(eta) > 1.5 && fabs(eta) < 2.0)
+    if (fabs(eta) >= 1.5 && fabs(eta) < 2.0)
         energy *= 91.19 / 90.46;
-    if (fabs(eta) > 2.0 && fabs(eta) < 2.5)
+    if (fabs(eta) >= 2.0 && fabs(eta) < 2.5)
         energy *= 91.19 / 90.03;
     
-    // finally scale to Z-Mass
+    // Abschliessende Skalierung aller Elektronen
     energy *= 91.19 / 91.28;
     
     return energy;
