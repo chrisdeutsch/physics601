80794e23bca133760c35564e6dc830216d23826d
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-03-26 21:50:11 +0100

    All new material

--- Positron-Lifetime/scripts/lifetime_fit.py
+++ Positron-Lifetime/scripts/lifetime_fit.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.special import erf
+from uncertainties import unumpy
+from uncertainties import correlated_values
 
 plt.style.use("publication")
 
@@ -31,187 +33,48 @@ x = np.linspace(700.0, 2000.0, 1000)
 # Fit results
 fit_res = []
 
+files = ["raumtemperatur",
+         "50_grad",
+         "62_grad",
+         "77_grad",
+         "88_grad",
+         "99_grad",
+         "110_grad",
+         "120_grad"]
 
-### Lifetime Raumtemperatur
-data = pd.read_csv("data/temperatures/raumtemperatur.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
+temp0 = [22.6, 50.0, 63.0, 76.0, 88.7, 100.3, 110.1, 121.3]
+temp1 = [22.8, 51.3, 62.6, 76.8, 88.9, 99.8, 110.0, 121.1]
 
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
+for f, T0, T1 in zip(files, temp0, temp1):
+    print("Processing: " + f)
+    
+    data = pd.read_csv("data/temperatures/{}.txt".format(f), sep="\t",
+                       index_col=0, names=["ch", "cnt"])
+    data["dcnt"] = np.sqrt(data.cnt)
+    
+    data = data[700:1600]
+    
+    plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
+    popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
+    perr = np.sqrt(np.diag(pcov))
+    plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
+    
+    (A0_, At_, tau0_, taut_) = correlated_values(popt[2:6], pcov[2:6,2:6])
+    
+    fit_res.append([T0, T1, popt[0], perr[0], popt[1], perr[1], popt[-1], perr[-1], A0_, At_, tau0_, taut_])
+    
+    plt.legend(loc=0)
+    plt.xlim(800, 1600)
+    plt.xlabel("Kanal")
+    plt.ylabel("Ereignisse~$N$")
 
-fit_res.append([22.6, 22.8, *popt.tolist(), *perr.tolist()])
+    plt.savefig("figures/lifetimes/{}.pdf".format(f))
+    plt.close()
 
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/raumtemperatur.pdf")
-plt.close()
-
-
-### Lifetime 50°C
-data = pd.read_csv("data/temperatures/50_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([50.0, 51.3, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/50.pdf")
-plt.close()
-
-
-### Lifetime 62°C
-data = pd.read_csv("data/temperatures/62_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([63.0, 62.6, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/62.pdf")
-plt.close()
-
-
-### Lifetime 77°C
-data = pd.read_csv("data/temperatures/77_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([76.0, 76.8, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/77.pdf")
-plt.close()
-
-
-### Lifetime 88°C
-data = pd.read_csv("data/temperatures/88_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([88.7, 88.9, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/88.pdf")
-plt.close()
-
-
-### Lifetime 99°C
-data = pd.read_csv("data/temperatures/99_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([100.3, 99.8, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/99.pdf")
-plt.close()
-
-
-### Lifetime 110°C
-data = pd.read_csv("data/temperatures/110_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([110.1, 110.0, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/110.pdf")
-plt.close()
-
-
-### Lifetime 120°C
-data = pd.read_csv("data/temperatures/120_grad.txt", sep="\t", index_col=0,
-                   names=["ch", "cnt"])
-data["dcnt"] = np.sqrt(data.cnt)
-data = data[700:2000]
-
-plt.errorbar(data.index, data.cnt, yerr=data.dcnt.get_values(), fmt="|", label="Lebenszeitspektrum")
-popt, pcov = curve_fit(lifetime, data.index, data.cnt, p0=[t0, sigma, A0, At, tau0, taut, bg])
-perr = np.sqrt(np.diag(pcov))
-plt.plot(x, lifetime(x, *popt), "-", linewidth=1.2, label="Anpassung")
-
-fit_res.append([121.3, 121.1, *popt.tolist(), *perr.tolist()])
-
-plt.legend(loc=0)
-plt.xlim(800, 1800)
-plt.xlabel("Kanal")
-plt.ylabel("Ereignisse~$N$")
-
-plt.savefig("figures/lifetimes/120.pdf")
-plt.close()
-
-
-t0, sigma, A0, At, tau0, taut, bg
 
 ### Lifetime table
-lifetimes = pd.DataFrame(fit_res, columns=["T0", "T1", "t0", "sigma", "A0", "At", "tau0", "taut", "bg", "dt0", "dsigma", "dA0", "dAt", "dtau0", "dtaut", "dbg"])
+lifetimes = pd.DataFrame(fit_res, columns=["T0", "T1", "t0", "dt0", "sigma", "dsigma", "bg", "dbg", "A0", "At", "tau0", "taut"])
+
 
 # Zeitkalibration
 m = 0.006721094760572852 # ns / Kanal
@@ -221,22 +84,33 @@ dm = 1.7194029982964498e-5
 m = 1000.0 * m
 dm = 1000.0 * dm
 
+
 # Mittlere Temperaturen
 lifetimes["Tbar"] = (lifetimes.T0 + lifetimes.T1) / 2.0
 lifetimes["dTbar"] = 0.0 * lifetimes.Tbar + 1.0
 
+# Kelvin
+#lifetimes.Tbar = lifetimes.Tbar + 273.15
+
 # Mittlere Lebenszeit (uncertainties zur Fehlerfortpflanzung)
-from uncertainties import unumpy
-A0 = unumpy.uarray(lifetimes.A0.get_values(), lifetimes.dA0.get_values())
-At = unumpy.uarray(lifetimes.At.get_values(), lifetimes.dAt.get_values())
-tau0 = unumpy.uarray(lifetimes.tau0.get_values(), lifetimes.dtau0.get_values())
-taut = unumpy.uarray(lifetimes.taut.get_values(), lifetimes.dtaut.get_values())
-
-taubar = A0 / (A0 + At) * tau0 + At / (A0 + At) * taut
-
-# Mittlere Lebenszeit
+taubar = lifetimes.A0 / (lifetimes.A0 + lifetimes.At) * lifetimes.tau0 + lifetimes.At / (lifetimes.A0 + lifetimes.At) * lifetimes.taut
 lifetimes["taubar"] = unumpy.nominal_values(taubar)
 lifetimes["dtaubar"] = unumpy.std_devs(taubar)
+
+
+# Uncertainties aus pandas frame entfernen
+A0 = lifetimes.A0.get_values()
+lifetimes["A0"], lifetimes["dA0"] = unumpy.nominal_values(A0), unumpy.std_devs(A0)
+
+At = lifetimes.At.get_values()
+lifetimes["At"], lifetimes["dAt"] = unumpy.nominal_values(At), unumpy.std_devs(At)
+
+tau0 = lifetimes.tau0.get_values()
+lifetimes["tau0"], lifetimes["dtau0"] = unumpy.nominal_values(tau0), unumpy.std_devs(tau0)
+
+taut = lifetimes.taut.get_values()
+lifetimes["taut"], lifetimes["dtaut"] = unumpy.nominal_values(taut), unumpy.std_devs(taut)
+
 
 ### Umrechnung von Kanal -> ps
 picosec = pd.DataFrame()
@@ -251,25 +125,63 @@ picosec["dtaubar"] = np.sqrt(lifetimes.taubar**2 * dm**2 + m**2 * lifetimes.dtau
 
 
 ### Mean lifetime plot
-plt.errorbar(picosec.Tbar, picosec.taubar, xerr=picosec.dTbar.get_values(), yerr=picosec.dtaubar.get_values(), fmt="o", label="mittlere Lebenszeit (Anpassung)")
+plt.errorbar(picosec.Tbar + 273.15, picosec.taubar, xerr=picosec.dTbar.get_values(), yerr=picosec.dtaubar.get_values(), fmt="o", label="mittlere Lebenszeit (Anpassung)")
 
-def s_curve(T, tauf, taut, sigma, S, H):
-    kB = 1.0
-    a = sigma * np.exp(S / kB - H / (kB * T))
-    return tauf * (1.0 + a * taut) / (1.0 + a * tauf)
+def s_curve(T, tauf, taut, a, b):
+    fac = a * np.exp(- b / (T + 273.15))
+    return tauf * (1.0 + fac * taut) / (1.0 + fac * tauf)
 
-popt, pcov = curve_fit(s_curve, picosec.Tbar, picosec.taubar, p0=[4.5e+2, 3.8e+2, 2.9e-1, -7.6, -190.0])
+mask = (picosec.Tbar < 60.0) | (picosec.Tbar > 70.0)
+popt, pcov = curve_fit(s_curve, picosec[mask].Tbar + 273.15, picosec[mask].taubar,
+                       p0=[322., 465., 186., 7000.],
+                       sigma=picosec[mask].dtaubar, absolute_sigma=True)
 
-x = np.linspace(20.0, 140.0, 1000)
+pcov[2:4, 2:4] = pcov[2:4, 2:4] * 0.0001
+
+x = np.linspace(280.0, 400.0, 1000)
 plt.plot(x, s_curve(x, *popt), "-", label="Anpassung S-Kurve")
 
-plt.xlabel(r"Temperatur~$T$ / \si{\degreeCelsius}")
+plt.xlabel(r"Temperatur~$T$ / \si{\kelvin}")
 plt.ylabel(r"mittlere Lebenszeit~$\bar{\tau}$ / \si{ps}")
 plt.legend(loc="lower right")
 
 
 plt.savefig("figures/lifetime_s_curve.pdf")
 plt.close()
+
+# Arrenius plot
+def log_sigc(T):
+    global pcov
+    ab = popt[2:4]
+    ab_cov = pcov[2:4, 2:4]
+    
+    (a, b) = correlated_values(ab, ab_cov)
+    
+    retval = unumpy.log(a) - b / T
+    return (unumpy.nominal_values(retval), unumpy.std_devs(retval))
+
+
+x = 1.0 / (picosec.Tbar + 273.15)
+dx = picosec.dTbar / (picosec.Tbar + 273.15)**2
+y, dy = log_sigc(picosec.Tbar + 273.15)
+
+
+plt.errorbar(x, y, xerr=dx, yerr=dy, fmt="o")
+
+popt, pcov = curve_fit(lambda x, m, b: m * x + b,
+                       x, y, sigma=dy, absolute_sigma=True)
+
+plt.plot(x, popt[0] * x + popt[1], "-")
+
+
+plt.xlabel(r"$\frac{1}{T}$ / \si{\per\kelvin}")
+plt.ylabel(r"$\ln(\sigma c_t)$")
+
+
+
+plt.savefig("figures/arrenius.pdf")
+plt.close()
+
 
 
 ### Latex Tabellen
