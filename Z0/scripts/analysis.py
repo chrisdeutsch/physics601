07a16d50d35ceb438a4b718abf719b710a8d86ad
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-05-08 13:55:13 +0200

    Ratio of hadrons to leptons

--- Z0/scripts/analysis.py
+++ Z0/scripts/analysis.py
@@ -205,6 +205,13 @@ sig_tau_e = tau_peak / e_peak
 r_mu_e = mu_part_fwhm / e_part_fwhm
 r_tau_e = tau_part_fwhm / e_part_fwhm
 
+# ratio of hadronic to lepton
+r_hadr_lepton = hadr_part_fwhm / (e_part_fwhm + mu_part_fwhm + tau_part_fwhm)
+
+
+
+
+
 # number of neutrino generations
 neutrino_fwhm = 0.1678 # GeV
 N_nu = (Z_fwhm - e_part_fwhm - mu_part_fwhm - tau_part_fwhm - hadr_part_fwhm) / neutrino_fwhm
