9381add115dae733e027965858b31f6200343c13
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-05-09 20:41:01 +0200

    Angularplots

--- Z0/scripts/s_t_plot.py
+++ Z0/scripts/s_t_plot.py
@@ -3,16 +3,33 @@ import matplotlib.pyplot as plt
 
 plt.style.use("publication")
 
+fig = plt.gcf()
+fig.set_size_inches(4.6, 5.0)
+
 costheta = np.linspace(-1.0, 1.0, 1000)
+s_channel = 1.0 + costheta**2
+t_channel = (1.0 - costheta)**-2
 
-plt.plot(costheta, 1.0 + costheta**2, "-", label="$s$-channel")
-plt.plot(costheta, (1.0 - costheta)**-2, "-", label="$t$-channel")
 
+ax1 = plt.subplot(211)
+plt.plot(costheta, s_channel, "-", label="$s$-channel (total)")
 
-plt.ylim((0, 5))
-plt.setp(plt.gca().get_yticklabels(), visible=False)
+plt.title(r"$\mathrm{e}^+ \mathrm{e}^- \rightarrow \mathrm{\mu}^+ \mathrm{\mu}^-$")
+plt.ylim((0, 3))
 plt.ylabel(r"$\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}$ / a.u.")
+plt.setp(ax1.get_xticklabels(), visible=False)
+plt.legend(loc=0)
+
+
+ax2 = plt.subplot(212, sharex=ax1)
+plt.plot(costheta, s_channel, "-", label="$s$-channel")
+plt.plot(costheta, t_channel, "-", label="$t$-channel")
+plt.plot(costheta, s_channel + t_channel, "-", label="total")
+
+plt.title(r"$\mathrm{e}^+ \mathrm{e}^- \rightarrow \mathrm{e}^+ \mathrm{e}^-$")
 plt.xlabel(r"$\cos\theta_\mathrm{W}$")
+plt.ylabel(r"$\frac{\mathrm{d}\sigma}{\mathrm{d}\Omega}$ / a.u.")
+plt.ylim((0, 10))
 plt.legend(loc=0)
 
 plt.tight_layout(pad=0.5)
