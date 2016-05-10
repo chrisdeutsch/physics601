861da7bdf950990c3f87dbbe8356c95ebd1828e7
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-05-10 14:13:15 +0200

    Histogramme angepasst

--- Z0/scripts/hist.py
+++ Z0/scripts/hist.py
@@ -7,14 +7,15 @@ plt.style.use("publication")
 files = ["ELECTRONS", "MUONS", "TAUS", "HADRONS"]
 data = dict((f, pd.read_csv("./data/tag1/" + f + ".csv")) for f in files)
 
-variables = [("Ctrk(Sump)", 10), ("Ctrk(N)", 10), ("Ecal(SumE)", 10), ("Hcal(SumE)", 10)]
+variables = [("Ctrk(Sump)", 10), ("Ctrk(N)", 15), ("Ecal(SumE)", 10), ("Hcal(SumE)", 10)]
 
 for var, b in variables:
     fig = plt.figure(figsize=(4.05, 3.4))
     sets = [data[key][var] for key in data]
-    plt.hist(sets, bins=b, label=[key.lower() for key in data])
-    plt.xlabel(var)
+    plt.hist(sets, bins=b, label=[key.lower() for key in data], edgecolor=None, linewidth=0.5)
+    plt.xlabel(r"\texttt{" + "{}".format(var) + "}")
     plt.legend(loc=0, framealpha=0.5)
     plt.minorticks_on()
     plt.tight_layout(pad=0.5)
     plt.savefig('./figures/event_display/'+str(var)+'.pdf')
+    plt.close()
